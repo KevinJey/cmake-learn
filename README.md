0c4463af8239cd8908eff5b6a0ad1c4ba6e3de94
junjiehu <junjiehu@MBP-2FP47RMD6P-0022.local>
2023-06-27 14:17:09 +0800

    [CMAKE] First Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# cmake-learn
-This repos is for learing cmake.
+This directory contains source code examples for the CMake Tutorial.
+Each step has its own subdirectory containing code that may be used as a
+starting point. The tutorial examples are progressive so that each step
+provides the complete solution for the previous step.
---
+++ Step1/build/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/junjiehu/Downloads/cmake-3.27.0-rc3-tutorial-source/Step1/tutorial.cxx" "CMakeFiles/Tutorial.dir/tutorial.cxx.o" "gcc" "CMakeFiles/Tutorial.dir/tutorial.cxx.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
